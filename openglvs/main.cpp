b6870f24eb1d9cc60909c2143cff58668806cad8
kevinNdyabandiho <59060080+kevinNdyabandiho@users.noreply.github.com>
2021-10-27 17:05:25 +0300

    Second commit [ background color ]

--- openglvs/main.cpp
+++ openglvs/main.cpp
@@ -11,8 +11,8 @@ int main(void)
         return -1;
     }
 
-    // Create a windowed mode window and its OpenGL context
-    window = glfwCreateWindow(640, 480, "2D Triangle", NULL, NULL);
+    // window and its context, window width and height
+    window = glfwCreateWindow(1000, 1000, "2D Triangle", NULL, NULL);
 
     if (!window)
     {
@@ -25,19 +25,20 @@ int main(void)
 
     float vertices[] =
     {
-        0, 0.5, 0.0, // top corner
+        0, 0.5, 0.0,     // top corner
         -0.5, -0.5, 0.0, // bottom left corner
-        0.5, -0.5, 0.0 // bottom right corner
+        0.5, -0.5, 0.0   // bottom right corner
     };
 
     GLfloat colour[] = {
         255, 0, 0, //red
         0, 255, 0, //green
-        0, 0, 255   //blue
+        0, 255, 0   //green
     };
-    /* Loop until the user closes the window */
+    /* Looping occurs until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        glClearColor(0.0,1.0,0.0,0.0);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Render OpenGL here
@@ -45,7 +46,7 @@ int main(void)
         glEnableClientState(GL_VERTEX_ARRAY); // tell OpenGL that you're using a vertex array for fixed-function attribute
         glEnableClientState(GL_COLOR_ARRAY);
         glVertexPointer(3, GL_FLOAT, 0, vertices); // point to the vertices to be used
-        glColorPointer(3, GL_FLOAT, 0, colour);
+        glColorPointer(3, GL_FLOAT, 0, colour);// point to the c
         glDrawArrays(GL_TRIANGLES, 0, 3); // draw the vertixes
         glDisableClientState(GL_VERTEX_ARRAY); // tell OpenGL that you're finished using the vertex arrayattribute
         glDisableClientState(GL_COLOR_ARRAY);
